2fff8463e95b159438246a739333c1f56e86b5bc
maxime-boizot <110637099+maxime-boizot@users.noreply.github.com>
2023-10-19 23:01:44 +0200

    ok

--- TP-s-linux/TP-3/resources/download.sh
+++
@@ -1,6 +0,0 @@
-[01/09/23 23:11:07] Video https://www.youtube.com/watch?v=pXaSFN9VYbw was downloaded. File path :
-/srv/yt/downloads/Women meme template/Women meme template.mp4
-[01/09/23 23:35:37] Video https://www.youtube.com/watch?v=aD-fFyGvDW8 was downloaded. File path :
-/srv/yt/downloads/cat/cat.mp4
-[01/09/23 23:38:45] Video https://www.youtube.com/watch?v=4cDqaLxrt6Q was downloaded. File path :
-/srv/yt/downloads/Vidéo courte et drôle/Vidéo courte et drôle.mp4
